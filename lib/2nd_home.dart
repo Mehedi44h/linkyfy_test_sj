987f1dcdf661539ea81497de69ac56d01dde7e70
mehedi hasan <{ID}+{username}@users.noreply.github.com>
2023-02-26 13:05:00 +0600

    done 2

--- lib/2nd_home.dart
+++ lib/2nd_home.dart
@@ -25,8 +25,9 @@ class _SecondHomeState extends State<SecondHome> {
       floatingActionButton: FloatingActionButton(
           onPressed: () {},
           child: CircleAvatar(
-            child: Image.asset('assets/images/pngwing 19.png'),
+            child: Image.asset('assets/images/pngwing 4.png'),
             radius: 30,
+            backgroundColor: Colors.grey.shade500,
           )
           //  Icon(Icons.add),
           ),
@@ -412,54 +413,52 @@ class _SecondHomeState extends State<SecondHome> {
                     Padding(
                       padding:
                           const EdgeInsets.only(left: 8.0, right: 8, top: 20),
-                      child: Center(
-                        child: Container(
-                          height: 265,
-                          width: 200,
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(15),
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.grey.withOpacity(1),
-                                // spreadRadius: 2,
-                                blurRadius: 3,
-                                offset: Offset(0, 2),
-                              ),
-                            ],
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Image.asset('assets/images/apple.png'),
-                                Text(
-                                  'Gshok Watch Bundle...',
-                                  style: TextStyle(
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                                Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Text(
-                                      'AED 99.0',
-                                      style: TextStyle(
-                                        fontSize: 20,
-                                        color: Colors.blue,
-                                      ),
-                                    ),
-                                    Icon(
-                                      Icons.favorite_border_outlined,
-                                      color: Colors.blue,
-                                    )
-                                  ],
-                                )
-                              ],
+                      child: Container(
+                        height: 265,
+                        width: 200,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(15),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(1),
+                              // spreadRadius: 2,
+                              blurRadius: 3,
+                              offset: Offset(0, 2),
                             ),
+                          ],
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Image.asset('assets/images/apple.png'),
+                              Text(
+                                'Gshok Watch Bundle...',
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    'AED 99.0',
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                      color: Colors.blue,
+                                    ),
+                                  ),
+                                  Icon(
+                                    Icons.favorite_border_outlined,
+                                    color: Colors.blue,
+                                  )
+                                ],
+                              )
+                            ],
                           ),
                         ),
                       ),
@@ -931,9 +930,12 @@ class _SecondHomeState extends State<SecondHome> {
                               )
                             ],
                           ),
-                          child: Image.asset(
-                            'assets/images/puma.png',
-                            fit: BoxFit.cover,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Image.asset(
+                              'assets/images/puma.png',
+                              fit: BoxFit.cover,
+                            ),
                           ),
                         ),
                         Padding(
@@ -952,9 +954,12 @@ class _SecondHomeState extends State<SecondHome> {
                                 ),
                               ],
                             ),
-                            child: Image.asset(
-                              'assets/images/puma.png',
-                              fit: BoxFit.cover,
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Image.asset(
+                                'assets/images/puma.png',
+                                fit: BoxFit.cover,
+                              ),
                             ),
                           ),
                         ),
@@ -972,9 +977,12 @@ class _SecondHomeState extends State<SecondHome> {
                               )
                             ],
                           ),
-                          child: Image.asset(
-                            'assets/images/puma.png',
-                            fit: BoxFit.cover,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Image.asset(
+                              'assets/images/puma.png',
+                              fit: BoxFit.cover,
+                            ),
                           ),
                         ),
                       ],
