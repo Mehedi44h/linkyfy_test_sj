086aa728d7d691534ffdea213ee39a1918bf38cd
mehedi hasan <{ID}+{username}@users.noreply.github.com>
2023-02-26 11:17:57 +0600

    final

--- lib/2nd_home.dart
+++ lib/2nd_home.dart
@@ -12,6 +12,7 @@ class SecondHome extends StatefulWidget {
 }
 
 class _SecondHomeState extends State<SecondHome> {
+  final TextEditingController searchcon = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,6 +23,375 @@ class _SecondHomeState extends State<SecondHome> {
       body: SingleChildScrollView(
         child: Column(
           children: [
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Icon(
+                    Icons.menu,
+                    size: 30,
+                  ),
+                  Image.asset(
+                    'assets/images/Group 9205.png',
+                    height: 26,
+                    width: 166,
+                  ),
+                  Container(
+                    height: 60,
+                    width: 60,
+                    decoration: BoxDecoration(
+                      color: Colors.blue.shade50,
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(width: 1, color: Colors.grey.shade300),
+                    ),
+                    child: Icon(
+                      Icons.notifications_none_rounded,
+                      size: 40,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: TextField(
+                controller: searchcon,
+                decoration: InputDecoration(
+                  prefixIcon: Icon(
+                    Icons.search_outlined,
+                    size: 30,
+                    color: Colors.grey,
+                  ),
+                  labelText: 'I am looking for...',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: 200,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.grey,
+                ),
+                child: Stack(children: [
+                  Positioned(left: 60, child: Text('50% off')),
+                  Image.asset(
+                    'assets/images/banner1.png',
+                    fit: BoxFit.cover,
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 30, top: 30, bottom: 30),
+                    child: Container(
+                      width: 150,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Season sale',
+                            style: TextStyle(
+                              fontSize: 28,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                          TextButton(
+                            onPressed: () {},
+                            child: Text(
+                              'Shop Now',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 17,
+                              ),
+                            ),
+                            style: ButtonStyle(
+                              backgroundColor:
+                                  MaterialStateProperty.all(Colors.cyanAccent),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    left: 150,
+                    bottom: 130,
+                    child: Center(
+                      child: Container(
+                        height: 60,
+                        width: 60,
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle, color: Colors.white),
+                        child: Center(
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 18, right: 10),
+                            child: Text(
+                              '50% off',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.w900,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ]),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 10.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Column(
+                    children: [
+                      CircleAvatar(
+                        radius: 30,
+                        backgroundColor: Colors.blue.shade100,
+                        child: Image.asset(
+                          'assets/images/beauty.png',
+                        ),
+                      ),
+                      Container(
+                        height: 45,
+                        width: 60,
+                        child: Text(
+                          'Brush',
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      CircleAvatar(
+                        radius: 30,
+                        backgroundColor: Colors.blue.shade100,
+                        child: Image.asset(
+                          'assets/images/gadget.png',
+                        ),
+                      ),
+                      Container(
+                        height: 45,
+                        width: 65,
+                        child: Text(
+                          'Gadget',
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      CircleAvatar(
+                        radius: 30,
+                        backgroundColor: Colors.blue.shade100,
+                        child: Image.asset(
+                          'assets/images/pngwing 1.png',
+                        ),
+                      ),
+                      Container(
+                        height: 45,
+                        width: 60,
+                        child: Text(
+                          'Smart Home',
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      CircleAvatar(
+                        radius: 30,
+                        backgroundColor: Colors.blue.shade100,
+                        child: Image.asset(
+                          'assets/images/pngwing 4.png',
+                        ),
+                      ),
+                      Container(
+                        height: 45,
+                        width: 60,
+                        child: Text(
+                          'Toys',
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      CircleAvatar(
+                        radius: 30,
+                        backgroundColor: Colors.blue.shade100,
+                        child: Image.asset(
+                          'assets/images/pngwing 5.png',
+                        ),
+                      ),
+                      Container(
+                        height: 45,
+                        width: 60,
+                        child: Text(
+                          'Baby Care',
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            // 2nd row
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Column(
+                    children: [
+                      CircleAvatar(
+                        radius: 30,
+                        backgroundColor: Colors.blue.shade100,
+                        child: Image.asset(
+                          'assets/images/pngwing 6.png',
+                        ),
+                      ),
+                      Container(
+                        height: 45,
+                        width: 60,
+                        child: Text(
+                          'Sports',
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      CircleAvatar(
+                        radius: 30,
+                        backgroundColor: Colors.blue.shade100,
+                        child: Image.asset(
+                          'assets/images/pngwing 7.png',
+                        ),
+                      ),
+                      Container(
+                        height: 45,
+                        width: 69,
+                        child: Text(
+                          'Fashion',
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      CircleAvatar(
+                        radius: 30,
+                        backgroundColor: Colors.blue.shade100,
+                        child: Image.asset(
+                          'assets/images/pngwing 10.png',
+                        ),
+                      ),
+                      Container(
+                        height: 45,
+                        width: 69,
+                        child: Text(
+                          'Season Sale',
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      CircleAvatar(
+                        radius: 30,
+                        backgroundColor: Colors.blue.shade100,
+                        child: Image.asset(
+                          'assets/images/pngwing 19.png',
+                        ),
+                      ),
+                      Container(
+                        height: 45,
+                        width: 75,
+                        child: Text(
+                          'Food Essential',
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      CircleAvatar(
+                        radius: 30,
+                        backgroundColor: Colors.blue.shade100,
+                        child: Image.asset(
+                          'assets/images/pngwing 5.png',
+                        ),
+                      ),
+                      Container(
+                        height: 45,
+                        width: 60,
+                        child: Text(
+                          'Baby Care',
+                          style: TextStyle(
+                              fontSize: 19, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            //  View all products
+            Padding(
+              padding: const EdgeInsets.only(
+                  left: 10.0, right: 10, top: 20, bottom: 7),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Deal of the Week',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  InkWell(
+                    onTap: () {
+                      Navigator.pushAndRemoveUntil(
+                          context,
+                          MaterialPageRoute(builder: (context) => SecondHome()),
+                          (route) => false);
+                    },
+                    child: Text(
+                      'View All Products',
+                      style: TextStyle(
+                        color: Colors.blue,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
             Padding(
               padding: const EdgeInsets.all(10.0),
               child: SingleChildScrollView(
@@ -1033,13 +1403,7 @@ class _SecondHomeState extends State<SecondHome> {
           ],
         ),
       ),
-      // floatingActionButton: FloatingActionButton(onPressed: () {}),
-      // floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       bottomNavigationBar: BottomAppBar(
-        // notchMargin: 5,
-        // elevation: 10,
-        // clipBehavior: Clip.antiAlias,
-        // shape: CircularNotchedRectangle(),
         child: BottomNavigationBar(
           items: [
             BottomNavigationBarItem(
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sojibtestp/2nd_home.dart';
 import 'package:sojibtestp/home%20page.dart';
 
 void main() {
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomePage(),
+      home:SecondHome(),
     );
   }
 }
